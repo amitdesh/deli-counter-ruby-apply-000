a996297678da50c451629969ee0380a2d6747087
A D <amitdesh2014@gmail.com>
2020-06-10 20:52:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,9 +20,5 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty?
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.shift}."
-  end
+  katz_deli.empty? "There is nobody waiting to be served!" : "Currently serving #{katz_deli.shift}."
 end
